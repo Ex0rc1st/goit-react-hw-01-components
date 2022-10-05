3cc659274bdbeb4d0f0e13a3cc36122b8089d1fd
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-10-05 16:40:08 +0300

    render task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
-
+import { PageHeader } from 'components/PageHeader/PageHeader';
+import { Profile } from 'components/Profile/Profile';
+import user from 'user.json';
 
 export const App = () => {
   return (
     <div>
-
+      <PageHeader text="goit-react-hw-01-components" />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,38 +1,43 @@
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-// export const Profile = props => {
-//     <div>
-//         <div>
-//             <img>
-//                 src={props.avatar}
-//                 alt={props.username}
-//             </img>
-//             <p>{props.username}</p>
-//             <p>{props.tag}</p>
-//             <p>{props.location}</p>
-//         </div>
+export const Profile = ({ username, tag, location, avatar, followers, views, likes }) => {
+    return (
+        <div class="profile">
+            <div class="description">
+                <img
+                src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+                alt="User avatar"
+                class="avatar"
+                />
+                <p class="name">Petra Marica</p>
+                <p class="tag">@pmarica</p>
+                <p class="location">Salvador, Brasil</p>
+            </div>
 
-//         <ul>
-//             <li>
-//                 <span>Followers</span>
-//                 <span>{props.stats.followers}</span>
-//             </li>
-//             <li>
-//                 <span>Views</span>
-//                 <span>{props.stats.views}</span>
-//             </li>
-//             <li>
-//                 <span>Likes</span>
-//                 <span>{props.stats.likes}</span>
-//             </li>
-//         </ul>
-//     </div>
-// };
+            <ul class="stats">
+                <li>
+                <span class="label">Followers</span>
+                <span class="quantity">1000</span>
+                </li>
+                <li>
+                <span class="label">Views</span>
+                <span class="quantity">2000</span>
+                </li>
+                <li>
+                <span class="label">Likes</span>
+                <span class="quantity">3000</span>
+                </li>
+            </ul>
+        </div>
+    );
+};
 
-// Profile.propTypes = {
-//     username: PropTypes.string.isRequired,
-//     tag: PropTypes.string.isRequired,
-//     location: PropTypes.string.isRequired,
-//     avatar: PropTypes.string.isRequired,
-//     stats: PropTypes.arrayOf().isRequired,
-// };
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+};
