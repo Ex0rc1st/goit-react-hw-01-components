86b7134248ac0502f2b9189bb00bece07b6188b6
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-10-05 14:59:23 +0300

    add data

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+// import PropTypes from "prop-types";
+
+// export const Profile = props => {
+//     <div>
+//         <div>
+//             <img>
+//                 src={props.avatar}
+//                 alt={props.username}
+//             </img>
+//             <p>{props.username}</p>
+//             <p>{props.tag}</p>
+//             <p>{props.location}</p>
+//         </div>
+
+//         <ul>
+//             <li>
+//                 <span>Followers</span>
+//                 <span>{props.stats.followers}</span>
+//             </li>
+//             <li>
+//                 <span>Views</span>
+//                 <span>{props.stats.views}</span>
+//             </li>
+//             <li>
+//                 <span>Likes</span>
+//                 <span>{props.stats.likes}</span>
+//             </li>
+//         </ul>
+//     </div>
+// };
+
+// Profile.propTypes = {
+//     username: PropTypes.string.isRequired,
+//     tag: PropTypes.string.isRequired,
+//     location: PropTypes.string.isRequired,
+//     avatar: PropTypes.string.isRequired,
+//     stats: PropTypes.arrayOf().isRequired,
+// };
