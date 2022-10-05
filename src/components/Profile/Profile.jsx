5b7dfffa475e0d74358ed3e6abb442bf2293bf12
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-10-05 17:06:17 +0300

    install  @emotion/styled @emotion/react

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,27 +5,27 @@ export const Profile = ({ username, tag, location, avatar, followers, views, lik
         <div class="profile">
             <div class="description">
                 <img
-                src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-                alt="User avatar"
+                src={avatar}
+                alt={username}
                 class="avatar"
                 />
-                <p class="name">Petra Marica</p>
-                <p class="tag">@pmarica</p>
-                <p class="location">Salvador, Brasil</p>
+                <p class="name">{username}</p>
+                <p class={tag}>@pmarica</p>
+                <p class="location">{location}</p>
             </div>
 
             <ul class="stats">
                 <li>
-                <span class="label">Followers</span>
-                <span class="quantity">1000</span>
+                    <span class="label">Followers</span>
+                    <span class="quantity">{followers}</span>
                 </li>
                 <li>
-                <span class="label">Views</span>
-                <span class="quantity">2000</span>
+                    <span class="label">Views</span>
+                    <span class="quantity">{views}</span>
                 </li>
                 <li>
-                <span class="label">Likes</span>
-                <span class="quantity">3000</span>
+                    <span class="label">Likes</span>   
+                    <span class="quantity">{likes}</span>
                 </li>
             </ul>
         </div>
